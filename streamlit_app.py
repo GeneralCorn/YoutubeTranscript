ee23817aa61db6e00580de203529e8d0f32bf08a
Cobbi <cobbi2004@gmail.com>
2024-08-19 00:37:31 +0800

    Srt Formatter

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,9 @@ from config import DEFAULT_API_KEY
 
 ################### Supporting Functions
 
+yt, bl = st.tabs(["Youtube", "Bilibili"])
+
+
 def get_video_title_and_views(url):
     try:
         response = requests.get(url)
@@ -112,60 +115,98 @@ def create_excel_file(channelid, is_playlist):
 
 ########## UI Start
 
-st.title("YouTube Transcript Generator")
-st.markdown("Note, a **channel without an ID** or **videos without transcripts** will be *skipped*")
-st.markdown("Channel Url Format: https://www.youtube.com/@username or https://www.youtube.com/{id}")
 
-option = st.radio(
-    "Select input type",
-    ["Enter YouTube Channel URLs", "Upload TXT file with URLs"],
-    index=0,
-    key="input_type",
-    help="Format: One full URL per line"
-)
 
-urls = []
-is_playlist = False
-if option == "Enter YouTube Channel URLs":
-    channel_url = st.text_area("Enter YouTube Channel URLs or Playlists (一次只用一种)")
-    urls = channel_url.splitlines()
-    if len(urls) >= 1:
-        if 'playlist' in urls[0]:
-            is_playlist = True
-elif option == "Upload TXT file with URLs":
-    uploaded_file = st.file_uploader("Upload a .txt file with YouTube Channel URLs", type=["txt"], key="file_upload")
+with yt:
+    st.title("Youtube Transcript Generator")
+    st.markdown("Note, a **channel without an ID** or **videos without transcripts** will be *skipped*")
+    st.markdown("Channel Url Format: https://www.youtube.com/@username or https://www.youtube.com/{id}")
+
+    option = st.radio(
+        "Select input type",
+        ["Enter YouTube Channel URLs", "Upload TXT file with URLs"],
+        index=0,
+        key="input_type",
+        help="Format: One full URL per line"
+    )
+
     urls = []
-    if uploaded_file is not None:
-        urls = uploaded_file.read().decode('utf-8').splitlines()
+    channel_url = ""
+    is_playlist = False
+    if option == "Enter YouTube Channel URLs":
+        channel_url = st.text_area("Enter YouTube Channel URLs or Playlists (一次只用一种)")
+        urls = channel_url.splitlines()
+        if len(urls) >= 1:
+            if 'playlist' in urls[0]:
+                is_playlist = True
+    elif option == "Upload TXT file with URLs":
+        uploaded_file = st.file_uploader("Upload a .txt file with YouTube Channel URLs", type=["txt"], key="file_upload")
+        urls = []
+        if uploaded_file is not None:
+            urls = uploaded_file.read().decode('utf-8').splitlines()
 
-video_details = {
-        'Views': [],
-        'Title': [],
-        'URL': [],
-        'Transcript': []
-}
+    video_details = {
+            'Views': [],
+            'Title': [],
+            'URL': [],
+            'Transcript': []
+    }
 
-channelid = {}
-if is_playlist:
-    for idx, url in enumerate(urls):
-        channelid[f"{idx}"] = url.split('playlist?list=')[-1]
-else:
-    for url in urls:
-        if "@" in url:
-            channelid[url.split('@')[1]] = get_channel_id(url.split('@')[1])
-        else:
-            channelid[url.split('channel/')[1]] = url.split('channel/')[1]
-
-#################### MAIN
-
-if st.button("Submit"):
-    if not urls:
-        st.error("Please provide at least one YouTube channel URL.")
+    channelid = {}
+    if is_playlist:
+        for idx, url in enumerate(urls):
+            channelid[f"{idx}"] = url.split('playlist?list=')[-1]
     else:
-        excel_file = create_excel_file(channelid, is_playlist)
-        st.download_button(
-            label="Download Excel file",
-            data=excel_file,
-            file_name="channel_data.xlsx",
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
+        for url in urls:
+            if "@" in url:
+                channelid[url.split('@')[1]] = get_channel_id(url.split('@')[1])
+            else:
+                channelid[url.split('channel/')[1]] = url.split('channel/')[1]
+
+    #################### MAIN
+
+    if st.button("Submit Link"):
+        if not urls:
+            st.error("Please provide at least one YouTube channel URL.")
+        else:
+            excel_file = create_excel_file(channelid, is_playlist)
+            st.download_button(
+                label="Download Excel file",
+                data=excel_file,
+                file_name="channel_data.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+    with bl:
+        st.title("Bilibili Transcript Formatter")
+
+        uploaded_file = st.file_uploader("Upload .srt files", type=["srt"], key="file_upload2", accept_multiple_files=True)
+
+        srt_dict = {}
+
+        if uploaded_file:
+            for file in uploaded_file:
+                content = file.read().decode("utf-8")
+                lines = content.splitlines()
+                chinese_text_lines = lines[2::4]
+                result = ", ".join(chinese_text_lines)
+                file_name = file.name.split(']')[1].split('.ai-zh.srt')[0]
+                srt_dict[file_name] = result
+
+        df = pd.DataFrame(list(srt_dict.items()), columns=['Name', 'Transcript'])
+
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False)
+
+        output.seek(0)
+
+        if st.button("Submit SRTs"):
+            if not uploaded_file:
+                st.error("Please upload at least one .srt file.")
+            else:
+                st.download_button(
+                        label="Download Excel file",
+                        data=output,
+                        file_name="bilibili_transcript.xlsx",
+                        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                    )
